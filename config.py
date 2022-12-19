6ca09669d85651c4a671cb2f0ca5877452f01c2d
ASalnikov <asalnikov1971@gmail.com>
2022-12-19 21:31:44 +0300

    Некоторые улучшения кода.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 TOKEN = '5957545018:AAEtHO8sg5IPxitmj8Ad7H15_MYCqYlT8J8'
 
 currencies = {
-        'Рубль'  : 'RUB',
-        'Евро'   : 'EUR',
-        'Доллар' : 'USD'
-        }
+              'Рубль'  : 'RUB',
+              'Евро'   : 'EUR',
+              'Доллар' : 'USD'
+             }
